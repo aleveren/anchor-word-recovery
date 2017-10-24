8e2c81901f2dff00b47a657d4018785732b9c413
Andrew Leverentz <aleverentz@noreply>
2017-10-24 00:59:23 -0700

    More python3 stuff

--- demo.sh
+++ demo.sh
@@ -7,15 +7,15 @@ do
     wget http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.$corpus.txt.gz
     gunzip docword.$corpus.txt.gz
     echo "preprocessing, translate from docword.txt to scipy format"
-    python uci_to_scipy.py docword.$corpus.txt M_$corpus.full_docs.mat
+    python3 uci_to_scipy.py docword.$corpus.txt M_$corpus.full_docs.mat
     echo "preprocessing: removing rare words and stopwords"
-    python truncate_vocabulary.py M_$corpus.full_docs.mat vocab.$corpus.txt 50
+    python3 truncate_vocabulary.py M_$corpus.full_docs.mat vocab.$corpus.txt 50
     for loss in L2
     do
         for K in 20 50 100
         do
             echo "learning with nonnegative recover method using $loss loss..."
-            python learn_topics.py M_$corpus.full_docs.mat.trunc.mat settings.example vocab.$corpus.txt.trunc $K $loss demo_$loss\_out.$corpus.$K
+            python3 learn_topics.py M_$corpus.full_docs.mat.trunc.mat settings.example vocab.$corpus.txt.trunc $K $loss demo_$loss\_out.$corpus.$K
         done
     done
 done
