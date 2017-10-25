6a6f3843ef30f97516eff035ba82412c6fbd0bfd
Andrew Leverentz <aleverentz@noreply>
2017-10-25 08:57:03 -0700

    Put top-level analysis routines into a class, and add an ipython notebook to inspect results

--- learn_topics.py
+++ learn_topics.py
@@ -8,13 +8,20 @@ import scipy.sparse as sparse
 import time
 from Q_matrix import generate_Q_matrix 
 import scipy.io
+from collections import namedtuple
+import argparse
 
-class Params:
+_Params_fields = ["log_prefix", "checkpoint_prefix", "seed", "eps",
+    "max_threads", "new_dim", "anchor_thresh", "top_words",
+    "infile", "vocab_file", "K", "loss", "outfile"]
 
-    def __init__(self, filename):
-        self.log_prefix=None
-        self.checkpoint_prefix=None
-        self.seed = int(time.time())
+class Params(namedtuple("Params", _Params_fields)):
+    @classmethod
+    def from_file_and_cmd_args(cls, filename, extra_args):
+        kwargs = dict(
+            log_prefix = None,
+            checkpoint_prefix = None,
+            seed = int(time.time()))
 
         with open(filename) as f:
             for l in f:
@@ -23,82 +30,117 @@ class Params:
                 l = l.strip()
                 l = l.split('=')
                 if l[0] == "log_prefix":
-                    self.log_prefix = l[1]
+                    kwargs["log_prefix"] = l[1]
                 elif l[0] == "max_threads":
-                    self.max_threads = int(l[1])
+                    kwargs["max_threads"] = int(l[1])
                 elif l[0] == "eps":
-                    self.eps = float(l[1])
+                    kwargs["eps"] = float(l[1])
                 elif l[0] == "checkpoint_prefix":
-                    self.checkpoint_prefix = l[1]
+                    kwargs["checkpoint_prefix"] = l[1]
                 elif l[0] == "new_dim":
-                    self.new_dim = int(l[1])
+                    kwargs["new_dim"] = int(l[1])
                 elif l[0] == "seed":
-                    self.seed = int(l[1])
+                    kwargs["seed"] = int(l[1])
                 elif l[0] == "anchor_thresh":
-                    self.anchor_thresh = int(l[1])
+                    kwargs["anchor_thresh"] = int(l[1])
                 elif l[0] == "top_words":
-                    self.top_words = int(l[1])
+                    kwargs["top_words"] = int(l[1])
 
-#parse input args
-if len(sys.argv) > 6:
-    infile = sys.argv[1]
-    settings_file = sys.argv[2]
-    vocab_file = sys.argv[3]
-    K = int(sys.argv[4])
-    loss = sys.argv[5]
-    outfile = sys.argv[6]
+        kwargs.update(extra_args)
 
-else:
-    print("usage: ./learn_topics.py word_doc_matrix settings_file vocab_file K loss output_filename")
-    print("for more info see readme.txt")
-    sys.exit()
+        return cls(**kwargs)
 
-params = Params(settings_file)
-params.dictionary_file = vocab_file
-M = scipy.io.loadmat(infile)['M']
-print("identifying candidate anchors")
-candidate_anchors = []
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile")
+    parser.add_argument("settings_file")
+    parser.add_argument("vocab_file")
+    parser.add_argument("K", type=int)
+    parser.add_argument("loss")
+    parser.add_argument("outfile")
+    args = parser.parse_args()
 
-#only accept anchors that appear in a significant number of docs
-for i in range(M.shape[0]):
-    if len(np.nonzero(M[i, :])[1]) > params.anchor_thresh:
-        candidate_anchors.append(i)
+    settings_file = args.settings_file
+    del args.settings_file
 
-print(len(candidate_anchors), "candidates")
+    params = Params.from_file_and_cmd_args(settings_file, extra_args = vars(args))
+    analysis = Analysis(params)
+    analysis.run()
 
-#forms Q matrix from document-word matrix
-Q = generate_Q_matrix(M)
+TopWordsSummary = namedtuple("TopWordsSummary",
+    ["topic_index", "anchor_word_index", "anchor_word",
+    "top_word_indices", "top_words"])
 
-with open(vocab_file) as f:
-    vocab = f.read().strip().split()
+class Analysis(object):
+    def __init__(self, params):
+        self.params = params
 
-#check that Q sum is 1 or close to it
-print("Q sum is", Q.sum())
-V = Q.shape[0]
-print("done reading documents")
+    def run(self):
+        params = self.params
+        M = scipy.io.loadmat(params.infile)['M']
+        print("identifying candidate anchors")
+        candidate_anchors = []
 
-#find anchors- this step uses a random projection
-#into low dimensional space
-anchors = findAnchors(Q, K, params, candidate_anchors)
-print("anchors are:")
-for i, a in enumerate(anchors):
-    print(i, vocab[a])
+        #only accept anchors that appear in a significant number of docs
+        for i in range(M.shape[0]):
+            if len(np.nonzero(M[i, :])[1]) > params.anchor_thresh:
+                candidate_anchors.append(i)
 
-#recover topics
-A, topic_likelihoods = do_recovery(Q, anchors, loss, params) 
-print("done recovering")
+        print(len(candidate_anchors), "candidates")
 
-np.savetxt(outfile+".A", A)
-np.savetxt(outfile+".topic_likelihoods", topic_likelihoods)
+        #forms Q matrix from document-word matrix
+        Q = generate_Q_matrix(M)
 
-#display
-with open(outfile+".topwords", 'w') as f:
-    for k in range(K):
-        topwords = np.argsort(A[:, k])[-params.top_words:][::-1]
-        print(vocab[anchors[k]], ':', end=' ')
-        print(vocab[anchors[k]], ':', end=' ', file=f)
-        for w in topwords:
-            print(vocab[w], end=' ')
-            print(vocab[w], end=' ', file=f)
-        print("")
-        print("", file=f)
+        with open(params.vocab_file) as f:
+            vocab = f.read().strip().split()
+
+        #check that Q sum is 1 or close to it
+        print("Q sum is", Q.sum())
+        V = Q.shape[0]
+        print("done reading documents")
+
+        #find anchors- this step uses a random projection
+        #into low dimensional space
+        anchors = findAnchors(Q, params.K, params, candidate_anchors)
+        print("anchors are:")
+        for i, a in enumerate(anchors):
+            print(i, vocab[a])
+
+        #recover topics
+        A, topic_likelihoods = do_recovery(Q, anchors, params.loss, params) 
+        print("done recovering")
+
+        np.savetxt(params.outfile+".A", A)
+        np.savetxt(params.outfile+".topic_likelihoods", topic_likelihoods)
+
+        #display
+        all_topwords = []
+        with open(params.outfile+".topwords", 'w') as f:
+            for k in range(params.K):
+                topwords = np.argsort(A[:, k])[-params.top_words:][::-1]
+                print(vocab[anchors[k]], ':', end=' ')
+                print(vocab[anchors[k]], ':', end=' ', file=f)
+                for w in topwords:
+                    print(vocab[w], end=' ')
+                    print(vocab[w], end=' ', file=f)
+                print("")
+                print("", file=f)
+                all_topwords.append(TopWordsSummary(
+                    topic_index = k,
+                    anchor_word_index = anchors[k],
+                    anchor_word = vocab[anchors[k]],
+                    top_word_indices = topwords,
+                    top_words = [vocab[w] for w in topwords]))
+
+        # make some results available as attributes of "self"
+        self.Q = Q
+        self.M = M
+        self.A = A
+        self.topic_likelihoods = topic_likelihoods
+        self.candidate_anchors = candidate_anchors
+        self.anchors = anchors
+        self.vocab = vocab
+        self.all_topwords = all_topwords
+
+if __name__ == "__main__":
+    main()
